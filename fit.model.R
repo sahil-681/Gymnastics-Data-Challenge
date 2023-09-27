1ed65555a1d562b64e25560bd02e10bddbe5b6f5
raylee92126 <raymond92126@gmail.com>
2023-09-27 14:48:40 -0400

    fixed fit.model.R NA columns issue

--- fit.model.R
+++ fit.model.R
@@ -14,11 +14,13 @@ stddevs_df <- data %>%
   pivot_wider(names_from=Apparatus, values_from=score_sd)
 
 events <- c("VT", "BB", "UB", "FX", "HB", "PB", "PH", "SR")
-samples_df <- as.data.frame(matrix(nrow=dim(means_df)[1], ncol=length(events)+3))
-colnames(samples_df) <- c(c("ID"), events)
+samples_df <- as.data.frame(matrix(nrow=dim(means_df)[1], ncol=length(events)))
+colnames(samples_df) <- events
+
 samples_df$ID <- means_df$ID
 samples_df$Country <- means_df$Country
 samples_df$Gender <- means_df$Gender
+
 
 # Loop through the means and standard deviations dataframes
 for (i in 1:nrow(means_df)) {
@@ -38,6 +40,7 @@ for (i in 1:nrow(means_df)) {
 }
 
 # write the means and stddevs output files
-write.csv(means_df, "data/means_per_app.csv", row.names = F)
-write.csv(stddevs_df, "data/stddevs_per_app.csv", row.names = F)
-write.csv(samples_df, "data/score_predictions.csv", row.names = F)
+# write.csv(means_df, "data/means_per_app.csv", row.names = F)
+# write.csv(stddevs_df, "data/stddevs_per_app.csv", row.names = F)
+# write.csv(samples_df, "data/score_predictions.csv", row.names = F)
+print(colnames(samples_df))
