d0de82d6db3fb0f602fd4ad005a14a8881772ad3
raylee92126 <raymond92126@gmail.com>
2023-10-02 11:35:30 -0400

    fixed get_reasonable_set, I was taking bottom 5 instead of top 5 that's embarrassing lol

--- get_reasonable_set.R
+++ get_reasonable_set.R
@@ -1,5 +1,4 @@
 get_reasonable_sets <- function(country, gender, means_df, stddevs_df, top_n=5){
-  
   if(gender == "m"){
     apps <- c("VT", "FX", "HB", "PB", "PH", "SR")
   }
@@ -9,10 +8,11 @@ get_reasonable_sets <- function(country, gender, means_df, stddevs_df, top_n=5){
   
   curr_means <- means_df[means_df$Country == country & means_df$Gender == gender, ]
   curr_stddevs <- stddevs_df[stddevs_df$Country == country & means_df$Gender == gender, ]
+  print(curr_means)
   
   reasonables <- numeric(length(apps)*top_n)
   for(i in 1:length(apps)){
-    sorted <- curr_means$ID[order(curr_means[, apps[i]])][1:top_n]
+    sorted <- curr_means$ID[order(curr_means[, apps[i]], decreasing = TRUE)][1:top_n]
     reasonables[top_n*(i-1)+c(1:top_n)] <- sorted
   }
   return(unique(reasonables))
