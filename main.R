13c307a8d90b9a78bc11c44b5c429d7c12bd8e4e
sahil-681 <sahil2001singh@gmail.com>
2023-10-02 14:01:18 -0400

    now calling get.best.teams from main

--- main.R
+++ main.R
@@ -1,9 +1,9 @@
 ## This script runs all code related to gymnastics case study. 
 ## It reads data and generates all outputs. 
-## main.R expects get.data.R, prep.data.R, and fit.model.R to be in the same directory level. It will import these files to create outputs.
+## main.R expects get.data.R, prep.data.R, fit.model.R and get.best.teams.R to be in the same directory level. It will import these files to create outputs.
 
 ## this script expects the current working directory to be the folder in which this file is located
- 
+
 # get.data.R data in the same directory level, inside which contains "data_2017_2021.csv" and "data_2022_2023.csv".
 source('get.data.R')
 
@@ -14,6 +14,10 @@ source('prep.data.R')
 source('fit.model.R')
 
 ## final script to get best teams and the US team simulations for all reasonable combinations 
-#source('get.best.teams.R') ## will take 1.5-2 hours to run
+source('get.best.teams.R')# outputs 2 lists (men & women) of 2 data frames each, 
+                          # one is optimized top 12 teams, other is all 
+                          # simulation results of country of interest (default: USA)
+                          # with apparatus-athlete pairs and descending order of preference
+                          # i.e. team on top is best
 
 print("finished")
