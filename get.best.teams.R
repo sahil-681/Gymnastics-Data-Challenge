a3475faa7a464b82cef7220f060911f09af05b62
sahil-681 <sahil2001singh@gmail.com>
2023-10-01 22:52:59 -0400

    updated working get.best.teams

--- get.best.teams.R
+++ get.best.teams.R
@@ -1,19 +1,7 @@
-# for each coubntry x, randomly sample all subsets from that country's reasonable set
-# for each subset, replace the current teams data to replace the currect X country's team
-# call simulate_medals (returns medalling_output table) for each subset and calculate their weighted medal score
-# calculate the weighted medaling score is 5-G, 3-S, 1-B
-# choose the team with the highest medaling score (if new medaling score higher than current medaling score then replace the current team with new team)
-# at the end edit the real list of top country athletes and replace the best team for each country
-# add weighting set as argument (50, 30, 15, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
-
-
 means = read.csv("data/means_per_app.csv")
 stddevs = read.csv("data/stddevs_per_app.csv")
 startteams = read.csv("data/team_country_qualified_individuals.csv")
 alt36 <- read.csv("data/mens_36_athletes.csv")
-
-#x <- get_reasonable_sets(country = "USA", gender = "m", means_df = means_df, 
-#                    stddevs_df = stddevs_df, top_n = 5)
 
 # helper function for replacing temp players for each combination
 replace_players <- function(start_teams, country, combinations, gender, i = 1) {
@@ -37,16 +25,16 @@ get.best.teams <- function(start_teams, countries, gender, means_df,
                            ctry_interest = "USA"){
   reasonable_set <- list()
   combinations <- list()
-  interested_sims <- data.frame(P1 = character(0),
-                                P2 = character(0),
-                                P3 = character(0),
-                                P4 = character(0),
-                                P5 = character(0),
-                                Gold = numeric(0),
-                                Silver = numeric(0),
-                                Bronze = numeric(0),
-                                TotalScore = numeric(0),
-                                MedalCount = numeric(0))
+  interested_sims <- data.frame(P1 = character(),
+                                P2 = character(),
+                                P3 = character(),
+                                P4 = character(),
+                                P5 = character(),
+                                Gold = numeric(),
+                                Silver = numeric(),
+                                Bronze = numeric(),
+                                TotalScore = numeric(),
+                                MedalCount = numeric())
   
   for (i in 1:length(countries)) {
     temp <- get_reasonable_sets(country = countries[i], gender = gender, 
@@ -99,27 +87,29 @@ get.best.teams <- function(start_teams, countries, gender, means_df,
       start_teams <- replace_players(start_teams, ctry, combinations, gender, l)
     }
     
-    # if (ctry == ctry_interest) {
-    #   interested_sims$P1[l] <- combinations[[ctry_interest]][l][[1]][1]
-    #   interested_sims$P2[l] <- combinations[[ctry_interest]][l][[1]][2]
-    #   interested_sims$P3[l] <- combinations[[ctry_interest]][l][[1]][3]
-    #   interested_sims$P4[l] <- combinations[[ctry_interest]][l][[1]][4]
-    #   interested_sims$P5[l] <- combinations[[ctry_interest]][l][[1]][5]
-    #   interested_sims$Gold[l] <- sim$Firsts[sim$Country == ctry_interest]
-    #   interested_sims$Silver[l] <- sim$Seconds[sim$Country == ctry_interest]
-    #   interested_sims$Bronze[l] <- sim$Thirds[sim$Country == ctry_interest]
-    #   interested_sims$TotalScore[l] <- sim$Score[sim$Country == ctry_interest]
-    #   interested_sims$MedalCount[l] <- sim$Firsts[[sim$Country == ctry_interest]] + sim$Seconds[[sim$Country == ctry_interest]]
-    # }
+    if (ctry == ctry_interest) {
+      interested_sims[l, "P1"] <- combinations[[ctry_interest]][l][[1]][1]
+      interested_sims[l, "P2"] <- combinations[[ctry_interest]][l][[1]][2]
+      interested_sims[l, "P3"] <- combinations[[ctry_interest]][l][[1]][3]
+      interested_sims[l, "P4"] <- combinations[[ctry_interest]][l][[1]][4]
+      interested_sims[l, "P5"] <- combinations[[ctry_interest]][l][[1]][5]
+      interested_sims[l, "Gold"] <- sim$Firsts[sim$Country == ctry_interest]
+      interested_sims[l, "Silver"] <- sim$Seconds[sim$Country == ctry_interest]
+      interested_sims[l, "Bronze"] <- sim$Thirds[sim$Country == ctry_interest]
+      interested_sims[l, "TotalScore"] <- sim$Score[sim$Country == ctry_interest]
+      interested_sims[l, "MedalCount"] <- sim$Firsts[sim$Country == ctry_interest] + sim$Seconds[sim$Country == ctry_interest]
+    }
   }
   return(list(finalteams = start_teams, interestedteam = interested_sims))
 }
 
+# example (takes close to 2 hours to run)
 z <- get.best.teams(start_teams = startteams, 
                     countries = unique(startteams$Country[startteams$Gender == "m"]),
                     gender = "m",
                     means, 
                     stddevs, alt36, ctry_interest = "USA")
 
-saveRDS(z, file = "best.teams.rds")
+# saved
+saveRDS(z, file = "data/best.teams.rds")
 
